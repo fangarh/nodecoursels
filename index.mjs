e12dd60922638c078d0b6ec9bb3bcd778e539e43
Dmitry <dvornikov.d@gmail.com>
2020-05-28 18:25:58 +0300

    promised files move added

--- index.mjs
+++ index.mjs
@@ -1,9 +1,11 @@
-import path from "path";
+import { FileOrgonizerPr } from "./task1promise.mjs";
 import { FileOrgonizer } from "./task1.mjs";
 
-const firstParam = process.argv.slice(2, 3)[0];
+const firstParam = process.argv.slice(2)[0];
+const secondParam = process.argv.slice(3)[0];
 if (
   !firstParam ||
+  !secondParam ||
   firstParam === "-h" ||
   firstParam === "-help" ||
   firstParam === "/?"
@@ -13,16 +15,29 @@ if (
   console.log();
   console.log("\tParameters:");
   console.log(
-    "\t\t[workPath], <[destPath], [skipExists], [deleteSrc], [deleteScanTimeout]>"
+    "\t\t [usePromises], [workPath], <[destPath], [skipExists], [deleteSrc], [deleteScanTimeout]>"
   );
   console.log();
-  console.log("\tworkPath - folder which will be scaned");
   console.log(
-    "\tdestPath - folder where copy files (default : 'workPath..Out')"
+    "\tusePromises - wich metod use: with or without promises. \n\t\tif usePromises is 'true' - promises will be used"
+  );
+  console.log("\tworkPath - folder which will be scaned\n");
+  console.log(
+    "\tdestPath - folder where copy files (default : 'workPath\\..\\Out')"
   );
   console.log(
     "\tskipExists - if file already exists in dest folder, it wont be copyed (default: true)"
   );
   console.log("\tdeleteSrc - delete source folder (default: false)");
   console.log("\tdeleteAwait - await in async folder delete (default: 1ms)");
-} else FileOrgonizer(...process.argv.slice(2, 6));
+} else {
+  if (firstParam === "true")
+    FileOrgonizerPr(...process.argv.slice(3))
+      .then(() => {
+        console.log("All done!!!!");
+      })
+      .catch((err) => {
+        console.log("Wah!!! Was Error!!!\n\n", err);
+      });
+  else FileOrgonizer(...process.argv.slice(3));
+}
