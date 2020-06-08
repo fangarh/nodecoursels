a15cc32830c7c420771063641666b4179599f774
Dmitry <dvornikov.d@gmail.com>
2020-06-08 09:31:27 +0300

    Task 3-4 template

---
+++ gulp/tasks/serve.js
@@ -0,0 +1,17 @@
+"use strict";
+
+module.exports = function () {
+  $.gulp.task("serve", function () {
+    $.browserSync.init({
+      open: false,
+      server: $.config.root,
+    });
+
+    $.browserSync.watch(
+      [$.config.root + "/**/*.*", "!**/*.css", "!**/*.html"],
+      $.browserSync.reload
+    );
+  });
+
+  console.log('here?');
+};
