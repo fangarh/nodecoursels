d778718a41f7a8bd4f3715c40019e99c6323ef45
Dmitry Dvornikov <dvornikov.d@gmail.com>
2020-06-29 15:37:31 +0300

    image loaded

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Redirect } from '@nestjs/common';
+import { Controller, Get, Redirect, Param, Res } from '@nestjs/common';
 
 const uri = 'http://localhost:3030/';
 
@@ -8,5 +8,12 @@ export class AppController {
   @Redirect(uri)
   redirectToClient(): void {
     return;
+  }
+
+  @Get('upload/:data')
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  uploadImg(@Param() p: any, @Res() res): any {
+    const img = p.data;
+    return res.sendFile(img, { root: './upload' });
   }
 }
